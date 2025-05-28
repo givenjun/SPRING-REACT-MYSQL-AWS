d8a4e974499167636719fbc6176648f74dbac3da
정준 <wjdwns0531@gmail.com>
2025-05-29 01:43:21 +0900

    2025-05-29 01:42 게시글 썸네일 & 이미지 개수 추가가

--- board-back/src/main/java/com/capstone/board_back/repository/resultSet/GetCommentListResultSet.java
+++ board-back/src/main/java/com/capstone/board_back/repository/resultSet/GetCommentListResultSet.java
@@ -1,6 +1,6 @@
 package com.capstone.board_back.repository.resultSet;
 
-public interface GetCommnetListResultSet {
+public interface GetCommentListResultSet {
 
     Integer getCommentNumber(); // ✨ 추가: 댓글 번호를 위한 getter
     String getUserEmail();    // ✨ 추가: 사용자 이메일을 위한 getter
